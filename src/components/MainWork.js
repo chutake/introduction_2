85c0491ced570f0e4de8b2fc9b0f0c3921cefacf
Takeru Chugun <takeru.chugun.3r@stu.hosei.ac.jp>
2022-11-19 17:14:56 +0900

    modified

--- src/components/MainWork.js
+++ src/components/MainWork.js
@@ -13,24 +13,20 @@ const MainWork = () => {
       <br />
 
       <h3>Javaで製作したもの</h3>
-      <ol>
-        <li type="1">
-          <a href="#section1">GUIプログラミング</a>
-        </li>
-        <li type="1">
-          <a href="#section2">s</a>
-        </li>
-      </ol>
+      <li>
+        <a href="#section1">GUIプログラミング</a>
+      </li>
+      <li>
+        <a href="#section2">s</a>
+      </li>
       <br />
       <h3>Pythonで製作したもの</h3>
-      <ol>
-        <li type="1">
-          <a href="#section4">エージェントシミュレーション</a>
-        </li>
-        <li type="1">
-          <a href="#section5">手書き数字認識</a>
-        </li>
-      </ol>
+      <li>
+        <a href="#section4">エージェントシミュレーション</a>
+      </li>
+      <li>
+        <a href="#section5">手書き数字認識</a>
+      </li>
 
       <li className="production">
         <h2>
