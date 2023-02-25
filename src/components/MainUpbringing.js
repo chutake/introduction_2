e516c06f9703255c0221daac5b3f71e4e89223b9
Takeru Chugun <takeru.chugun.3r@stu.hosei.ac.jp>
2023-02-25 17:32:28 +0900

    modified and add upbringing

--- src/components/MainUpbringing.js
+++ src/components/MainUpbringing.js
@@ -5,9 +5,13 @@ class MainUpbringing extends React.Component {
     return (
       <div className="main-upbringing">
         <h1>Upbringing</h1>
-        <div className="container">
-          <h6>----法政大学情報科学部コンピュータ科学科　中郡健竜----</h6>
-        </div>
+        <ul>
+          <li>2001年　誕生!!</li>
+          <li>2017年4月　都立江北高等学校　入学</li>
+          <li>2020年3月　都立江北高等学校　卒業</li>
+          <li>2020年4月　法政大学情報科学部コンピュータ科学科　入学</li>
+          <li>2024年3月　法政大学情報科学部コンピュータ科学科　卒業予定</li>
+        </ul>
       </div>
     );
   }
